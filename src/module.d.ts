dca150016cd187680bc388c30c07689ef02056ef
leishuai <1757768548@qq.com>
2022-08-10 20:59:07 +0800

    修改bug,删除多余的说明文件

--- src/module.d.ts
+++ src/module.d.ts
@@ -4,13 +4,6 @@ declare module '*.module.less' {
   };
   export default classess;
 }
-
-declare module 'colorpicker-v3' {
-  import ColorpickerV3 from 'colorpicker-v3';
-  const colorpickerV3: ColorpickerV3;
-  export default colorpickerV3;
-}
-
 declare module '*.vue' {
   import { ComponentOptions } from 'vue';
   const Component: ComponentOptions;
